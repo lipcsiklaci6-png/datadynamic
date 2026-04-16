2b8aa23950e2faefac5af1b251b6c60b6ba544a3
lipcsiklaci6-png <lipcsiklaci6@gmail.com>
2026-04-16 13:25:58 +0000

    fix: move static build to root for easy deployment

--- datadynamic-web/next.config.ts
+++ datadynamic-web/next.config.ts
@@ -6,6 +6,8 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/datadynamic',
+  assetPrefix: '/datadynamic',
 };
 
 export default withNextIntl(nextConfig);
